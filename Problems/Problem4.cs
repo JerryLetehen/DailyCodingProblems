722475f8d644255673123410007065ea1f9431b5
RuslanDudnikov <jerryletehen@gmail.com>
2020-04-25 10:47:22 +0300

    solve Problem4

---
+++ Problems/Problem4.cs
@@ -0,0 +1,60 @@
+using System;
+
+namespace DailyCodingProblems
+{
+    /*
+     * Given an array of integers, find the first missing positive integer in linear time and constant space.
+     * In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
+     * For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+     * You can modify the input array in-place.
+     */
+
+    public class Problem4 : IProblem
+    {
+        private int[] array;
+
+        public Problem4(int[] array)
+        {
+            this.array = array;
+        }
+
+        public void Solve()
+        {
+            int result = GetFirstMissingPositive(array);
+            PrintResult(result);
+        }
+
+        private int GetFirstMissingPositive(int[] nums)
+        {
+            Array.Sort(nums);
+            int result = 1;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var number = nums[i];
+                if (number <= 0)
+                {
+                    continue;
+                }
+
+                if (number > result)
+                {
+                    return result;
+                }
+
+                if (number < result)
+                {
+                    continue;
+                }
+
+                result++;
+            }
+
+            return result;
+        }
+
+        private void PrintResult(int result)
+        {
+            Console.WriteLine("Result : " + result);
+        }
+    }
+}
